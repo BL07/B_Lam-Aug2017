7316a0d2c4a3d77c7bf7eedda47022c755b79296
BL07 <18lamb43@millshighsch.org>
2017-08-22 09:28:37 -0700

    Second Project -- using Git from Eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,15 @@
+
+//Bryan Lam
+//my second java project. It will compare a print and printIn.
+//August 22, 2017
+
+public class Project2 {
+
+public static void main(String[] args) {
+	System.out.println("Hello -- this is project 2!!!");
+	
+	System.out.print("Hello -- this is project 2!!!");
+
+	}
+
+}
